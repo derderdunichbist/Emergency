826e7ce9fc2bf4de19e6e7069b60ea5b55457be8
Fabian <fabian.brenner@student.reutlingen-university.de>
2016-05-31 15:56:00 +0200

    Function of a widget, an a message service
    but i cant start with davids changes

---
+++ app/src/main/java/org/hsrt/mc/emergency/backend/AppWidget.java
@@ -0,0 +1,43 @@
+package org.hsrt.mc.emergency.backend;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import org.hsrt.mc.emergency.R;
+
+/**
+ * Created by Fabian on 31.05.2016.
+ */
+public class AppWidget extends AppWidgetProvider {
+
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){ // Update Widget
+        // context.startService(new Intent(context, SendingService.class));
+
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, SendingService.class);
+            PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0); // Pending Intent for the sendingService
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setOnClickPendingIntent(R.id.emergencyButton, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
---
+++ app/src/main/java/org/hsrt/mc/emergency/backend/SendingService.java
@@ -0,0 +1,39 @@
+package org.hsrt.mc.emergency.backend;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import org.hsrt.mc.emergency.R;
+
+/**
+ * Created by Fabian on 31.05.2016.
+ */
+public class SendingService extends IntentService {
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     */
+    public SendingService() {
+        super(SendingService.class.getName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        // build notification (icon,title,text)
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.emerg_button)
+                        .setContentTitle("Emergency-Call")
+                        .setContentText("Your Emergency Call is send");
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //show notification
+        mNotificationManager.notify(123, mBuilder.build());
+    }
+}
