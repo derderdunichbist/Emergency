2e762e2904a6cc3e5d3c5f7f3a96ed90fbe07810
Konstantinos <konstantinos.anagnostou@student.reutlingen-university.de>
2016-06-09 16:54:45 +0200

    Added the settings menu, yet with no functionality

--- app/src/main/java/org/hsrt/mc/emergency/activities/MainActivity.java
+++ app/src/main/java/org/hsrt/mc/emergency/activities/MainActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -206,6 +207,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.addMarker(new MarkerOptions().position(hochschule).title("Mein Standort"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(hochschule));
         mMap.animateCamera(zoom);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
--- app/src/main/java/org/hsrt/mc/emergency/activities/MapsOwnPosition.java
+++ app/src/main/java/org/hsrt/mc/emergency/activities/MapsOwnPosition.java
@@ -1,4 +1,4 @@
-package org.hsrt.mc.emergency;
+package org.hsrt.mc.emergency.activities;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -9,6 +9,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.hsrt.mc.emergency.R;
 
 public class MapsOwnPosition extends FragmentActivity implements OnMapReadyCallback {
 
