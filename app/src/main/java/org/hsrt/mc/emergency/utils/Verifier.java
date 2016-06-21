2785bee91776ab2fb201de1a206fa615ca6f01ca
Konstantinos <konstantinos.anagnostou@student.reutlingen-university.de>
2016-06-21 13:35:12 +0200

    Added Verifier class

---
+++ app/src/main/java/org/hsrt/mc/emergency/utils/Verifier.java
@@ -0,0 +1,30 @@
+package org.hsrt.mc.emergency.utils;
+
+/**
+ * Created by KA on 21.06.2016.
+ */
+public class Verifier {
+
+    public static boolean isStringEmptyOrNull(String string) {
+        boolean isEmptyOrNull;
+        if(string == null || string.isEmpty()) {
+            isEmptyOrNull =  false;
+        } else {
+            isEmptyOrNull = true;
+        }
+
+        return isEmptyOrNull;
+    }
+
+    public static boolean isIntZero(int amountPerDay) {
+        boolean isIntZero;
+
+        if(amountPerDay == 0) {
+            isIntZero = true;
+        } else {
+            isIntZero = false;
+        }
+
+        return isIntZero;
+    }
+}
