9c5cb159d569f0806e7ae6d91b0a6cc02057abfb
Fabian <fabian.brenner@student.reutlingen-university.de>
2016-06-28 13:27:16 +0200

    little changes on sms send,
    NOTE: only  max. 160 chars in one sms

--- app/src/main/java/org/hsrt/mc/emergency/services/SendingService.java
+++ app/src/main/java/org/hsrt/mc/emergency/services/SendingService.java
@@ -13,7 +13,11 @@ import android.telephony.SmsManager;
 import android.widget.Toast;
 
 import org.hsrt.mc.emergency.R;
+import org.hsrt.mc.emergency.user.Contact;
+import org.hsrt.mc.emergency.user.UserImplementation;
 import org.hsrt.mc.emergency.utils.UserMessage;
+
+import java.util.List;
 
 /**
  * Created by Fabian on 31.05.2016.
@@ -30,32 +34,44 @@ public class SendingService extends IntentService {
     protected void onHandleIntent(Intent intent) { //Sending Service Handle with invoke of an intent
 
         UserMessage userMessage = new UserMessage(this.getApplicationContext());
+        List<Contact> contacts = UserImplementation.getUserObject().getContacts();
+        String favoriteNumber = "";
+
+
+
 
         // build notification (icon,title,text)
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("Emergency-Call")
-                        .setContentText(userMessage.getEmergencyMessage());
+                        .setContentText(userMessage.getSuccessful().toString());
 
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         //show notification
         mNotificationManager.notify(123, mBuilder.build()); // Show an build notification with id=123
 
-         // send SMS-Message
-        String text = userMessage.getEmergencyMessage();
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage("015787405462", null, "EMERGENCY-SMS: " + text, null, null); // send Message, put your test number here
+         // send SMS-Messages
+        for (int i = 0; i < contacts.size(); i++) {
+
+            String text = userMessage.getEmergencyMessage();
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(contacts.get(i).getPhoneNumber(), null, text, null, null);
+            if(contacts.get(i).isFavourite()){
+                favoriteNumber = contacts.get(i).getPhoneNumber();
+            }
+        }
+
 
         // // Make Phone-Call Intent
-       // Intent callIntent = new Intent(Intent.ACTION_CALL);
-       // callIntent.setData(Uri.parse("tel:015787405462"));
-       // callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:"+favoriteNumber));
+        callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
 
        try{
-       //    startActivity(callIntent);
+           startActivity(callIntent);
        }
        catch(Exception e){
            System.out.print(e.getMessage());
